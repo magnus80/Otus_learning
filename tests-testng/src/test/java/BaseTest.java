2a40fcfce920520c1cc9984dacbc0d54d07b876a
konstantin.iryshkov <zarechny@inbox.ru>
2020-01-22 20:35:41 +0300

    исправлен тест и логгер #2

--- tests-testng/src/test/java/BaseTest.java
+++ tests-testng/src/test/java/BaseTest.java
@@ -2,31 +2,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.lang.reflect.Method;
 
 import static java.util.Arrays.asList;
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class BaseTest {
 
     private WebDriver driver;
     private static final Logger logger = LogManager.getLogger(BaseTest.class.getName());
 
-    @BeforeTest
+    @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver.manage().timeouts().implicitlyWait(10, SECONDS);
     }
 
     @BeforeMethod(alwaysRun = true)
     public void loggingSetUp(Method m, Object[] p) {
         logger.info("Start test: " + m.getName() + " with parameters " + asList(p));
-        driver.get("http://otus.ru");
     }
 
     @AfterMethod(alwaysRun = true)
@@ -34,7 +31,7 @@ public class BaseTest {
         logger.info("End test: " + m.getName());
     }
 
-    @AfterTest
+    @AfterClass
     public void tearDown() {
         if (driver != null)
             driver.quit();
